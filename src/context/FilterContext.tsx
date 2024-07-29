abc562b35841397025b2e5e9ff795e7ff7d26cd7
NatnaelDemelash <natnael.dev101@gmail.com>
2024-07-29 14:17:07 +0300

    created a filter context

---
+++ src/context/FilterContext.tsx
@@ -0,0 +1,59 @@
+import {
+  createContext,
+  useContext,
+  useState,
+  ReactNode,
+  Children,
+} from "react";
+
+interface FilterContextType {
+  searchQuery: string;
+  setSearchQuery: (query: string) => void;
+  selectedCategory: string;
+  setSelectedCategory: (category: string) => void;
+  minPrice: number | undefined;
+  setMinPrice: (price: number | undefined) => void;
+  maxPrice: number | undefined;
+  setMaxPrice: (price: number | undefined) => void;
+  keword: string;
+  setKeyword: (keyword: string) => void;
+}
+
+const FilterContext = createContext<FilterContextType | undefined>(undefined);
+
+export const FilterProvider = (Children: ReactNode) => {
+  const [searchQuery, setSearchQuery] = useState<string>("");
+  const [selectedCategory, setSelectedCategory] = useState<string>("");
+  const [minPrice, setMinPrice] = useState<number | undefined>(undefined);
+  const [maxPrice, setMaxPrice] = useState<number | undefined>(undefined);
+  const [keword, setKeyword] = useState<string>("");
+
+  return (
+    <FilterContext.Provider
+      value={{
+        searchQuery,
+        setSearchQuery,
+        selectedCategory,
+        setSelectedCategory,
+        minPrice,
+        setMinPrice,
+        maxPrice,
+        setMaxPrice,
+        keword,
+        setKeyword,
+      }}
+    >
+      {Children}
+    </FilterContext.Provider>
+  );
+};
+
+export const useFilter = () => {
+  const context = useContext(FilterContext);
+
+  if (context === undefined) {
+    throw new Error("Use filter must be used within a FilterProvider");
+  }
+
+  return context;
+};
